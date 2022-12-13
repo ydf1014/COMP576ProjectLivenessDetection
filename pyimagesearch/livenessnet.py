0d1d924b8b534bf860f8d1c9041821c03265f5e3
Qiyusi98 <34912636+Qiyusi98@users.noreply.github.com>
2022-12-13 13:41:35 +0800

    livenessnet
    build livenessnet, VGG16, InceptionV3, ResNet50

--- pyimagesearch/livenessnet.py
+++ pyimagesearch/livenessnet.py
@@ -1,30 +1,27 @@
-# import the necessary packages
-from keras.models import Sequential
-from tensorflow.keras.layers import BatchNormalization
+import tensorflow as tf
+from keras.layers import BatchNormalization
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dropout
-from keras.layers.core import Dense
+from keras.layers import Activation
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers import Dense
+from keras.layers.pooling import AveragePooling2D
 from keras import backend as K
+from keras.applications import VGG16, InceptionV3, ResNet50
+
 
 class LivenessNet:
 	@staticmethod
 	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last" and the channels dimension itself
-		model = Sequential()
+		model = tf.keras.Sequential()
 		inputShape = (height, width, depth)
 		chanDim = -1
 
-		# if we are using "channels first", update the input shape
-		# and channels dimension
 		if K.image_data_format() == "channels_first":
 			inputShape = (depth, height, width)
 			chanDim = 1
 
-		# first CONV => RELU => CONV => RELU => POOL layer set
 		model.add(Conv2D(16, (3, 3), padding="same",
 			input_shape=inputShape))
 		model.add(Activation("relu"))
@@ -35,7 +32,6 @@ class LivenessNet:
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.25))
 
-		# second CONV => RELU => CONV => RELU => POOL layer set
 		model.add(Conv2D(32, (3, 3), padding="same"))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization(axis=chanDim))
@@ -45,16 +41,57 @@ class LivenessNet:
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Dropout(0.25))
 
-		# first (and only) set of FC => RELU layers
 		model.add(Flatten())
 		model.add(Dense(64))
 		model.add(Activation("relu"))
 		model.add(BatchNormalization())
 		model.add(Dropout(0.5))
 
-		# softmax classifier
 		model.add(Dense(classes))
 		model.add(Activation("softmax"))
 
-		# return the constructed network architecture
 		return model
+
+
+class Liveness_VGG16:
+    @staticmethod
+    def build(width, height, depth, classes):
+        input_shape = (height,width,depth)
+        model = VGG16(weights="imagenet", include_top=False, input_shape= input_shape)
+        model.trainable = False
+        inputs = tf.keras.Input(shape= input_shape)
+        x = model(inputs, training=False)
+        x = Flatten()(x)
+        x = Dense(50, activation="relu")(x)
+        outputs = Dense(classes, activation="softmax")(x)
+        model = tf.keras.Model(inputs, outputs)
+        return model
+
+class Liveness_InceptionV3:
+    @staticmethod
+    def build(width, height, depth, classes):
+        input_shape = (height,width,depth)
+        model = InceptionV3(weights="imagenet", include_top=False, input_shape= input_shape)
+        model.trainable = False
+        inputs = tf.keras.Input(shape= input_shape)
+        x = model(inputs, training=False)
+        x = Flatten()(x)
+        x = Dense(50, activation="relu")(x)
+        outputs = Dense(classes, activation="softmax")(x)
+        model = tf.keras.Model(inputs, outputs)
+        return model
+
+class Liveness_ResNet50:
+    @staticmethod
+    def build(width, height, depth, classes):
+        input_shape = (height,width,depth)
+        model = ResNet50(weights="imagenet", include_top=False, input_shape=input_shape)
+        model.trainable = False
+        inputs = tf.keras.Input(shape=input_shape)
+        x = model(inputs, training=False)
+        x = Flatten()(x)
+        x = Dense(50, activation="relu")(x)
+        outputs = Dense(classes, activation="softmax")(x)
+        model = tf.keras.Model(inputs, outputs)
+        return model
+
